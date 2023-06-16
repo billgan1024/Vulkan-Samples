e271b673b7e1f0cb251725a49788e8c883825904
Bill Gan <billgan12345@gmail.com>
2023-06-16 02:16:05 -0400

    hi

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # source this file to build the samples and set up the environment
 cmake -G "Unix Makefiles" -H. -Bbuild/linux -DCMAKE_BUILD_TYPE=Release
 cmake --build build/linux --config Release --target vulkan_samples -j$(nproc)
-alias vulkan-samples="build/linux/app/bin/Release/x86_64/vulkan_samples"
+alias vulkan_samples="build/linux/app/bin/Release/x86_64/vulkan_samples"
